df56bbb7d9fc824939f2a73f5fecf97571f49d22
Robert Pliszka <robplisz@gmail.com>
2022-10-17 19:26:51 -0400

    Added comment to the race.js

--- race.js
+++ race.js
@@ -1,13 +1,20 @@
+// Race number and time schedule
+
+// Generates random number
 let raceNumber = Math.floor(Math.random() * 1000);
 
+// User registered early
 var earlyRegister = true;
 
+// Racer age
 var racerAge = 36;
 
+// Checks if user has registered early
 if (earlyRegister === true && racerAge > 18) {
   raceNumber += 1000;
 }
 
+// Checks which category the racer qualifis for.
 if (racerAge > 18 && earlyRegister === true) {
   console.log(
     `Your race starts at 9:30 AM and your race number is ${raceNumber}`
